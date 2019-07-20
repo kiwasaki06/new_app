c036a1f18a0654e7f592ff66c64b24fb1c3ec594
koji iwasaki <jamjamjamjam666@gmail.com>
2019-07-20 13:46:59 +0900

    devise user_controllerを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190719075614) do
+ActiveRecord::Schema.define(version: 20190720020429) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190719075614) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
